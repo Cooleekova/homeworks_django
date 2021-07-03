1ea3baade63aa098a959ef47d2fa7307224b7937
Atya <nushnushka@gmail.com>
2021-07-03 23:32:09 +0300

    dockerfile and docker-compose were added

--- drf-intro/simple_crud/simple_crud/settings.py
+++ drf-intro/simple_crud/simple_crud/settings.py
@@ -77,17 +77,27 @@ WSGI_APPLICATION = 'simple_crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'drf_homework',
+#         'USER': 'atya',
+#         'PASSWORD': '14091989',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'drf_homework',
-        'USER': 'ЮЗЕР',
-        'PASSWORD': 'ПАССВОРД',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
